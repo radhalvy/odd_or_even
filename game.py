5efcea3410de2f5ea544c46412581bd02e5b05c1
Radhalvy Bernabel <radhalvybernabel@gmail.com>
2021-07-25 12:11:02 -0400

    Finished the loop creating new two lists and formatted a little bit.

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+positive_answers = ["y", "yes", "Y", "Yes", "si", "Si", "s", "S"]
+negative_answers = ["n", "N", "no", "No"]
 def odd_even():
     following_loop = True
     while following_loop == True:
@@ -8,23 +10,29 @@ def odd_even():
             return
         zero_or_one = user_num % 2
         if zero_or_one == 0:
-            print("The number you entered is even.")
+            print("The number you entered is even.\n")
             print("Do you want to enter another number?")
             continue_or_not = input("Answer: ")
-            if continue_or_not != "yes":
-                print("Goodbye.")
+            if continue_or_not in positive_answers:
+                following_loop = True
+            elif continue_or_not in negative_answers:
+                print("\nGoodbye.")
                 following_loop = False
             else:
-                following_loop = True
+                print("\nInvalid answer. Closing the program...")
+                following_loop = False
         else:
-            print("The number you entered is odd.")
+            print("The number you entered is odd.\n")
             print("Do you want to enter another number?")
             continue_or_not = input("Answer: ")
-            if continue_or_not != "yes":
-                print("Goodbye.")
+            if continue_or_not in positive_answers:
+                following_loop = True
+            elif continue_or_not in negative_answers:
+                print("\nGoodbye.")
                 following_loop = False
             else:
-                following_loop = True
+                print("\nInvalid answer. Closing the program...")
+                following_loop = False
 
 
 odd_even()
