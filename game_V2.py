88862f4672767c1a444479cba5d3fcfc6c6d15f0
Radhalvy Bernabel <radhalvybernabel@gmail.com>
2021-08-02 23:17:47 -0400

    Deleted some variables that weren't doing anything and made the code a little bit cleaner

--- game_V2.py
+++ game_V2.py
@@ -1,24 +1,26 @@
 positive_answers = ["Y", "YES", "S", "SI"]
 negative_answers = ["N", "NO"]
-user_num = ""
+
 
 def get_input():
     try:
         user_num = int(input("Enter the number: ")) % 2
     except ValueError:
-        print("Invalid integer.")
+        print("\nInvalid integer.\n")
         return
     return user_num
 
 
-def odd_even(user_input_function):
+def odd_even():
     state = True
     while state:
-        user_input_function = get_input()
-        if user_input_function == 1:
+        user_num = get_input()
+        if user_num == 1:
             print("\nThe number is odd.")
-        else:
+        elif user_num == 0:
             print("\nThe number is even.")
+        else:
+            return
         print("\nDo you want to enter another number?")
         another_num_user = input("\nAnswer: ")
         if another_num_user.upper() in positive_answers:
@@ -27,6 +29,7 @@ def odd_even(user_input_function):
             state = False
         else:
             print("\nInvalid answer. Closing the program...")
+            return
 
 
-odd_even(user_num)
+odd_even()
